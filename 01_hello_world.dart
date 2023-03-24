a5f530371f08b6643f8eb1e4f8706992088ffb07
Angel Travieso <angeltraviesoc@gmail.com>
2023-03-24 17:05:07 -0400

    hola mundo - Dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,9 @@
+void main() {
+  // var myName = 'Angel';
+  // String myName = 'Angel';
+  // late final myName;
+  const myName = 'Angel';
+
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+}

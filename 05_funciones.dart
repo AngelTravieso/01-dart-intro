92f25697b226ceceb1d8736f054f9f41e03348bf
atravieso <angeltraviesoc@gmail.com>
2023-06-18 17:30:25 -0400

    Parametros con funciones

--- 05_funciones.dart
+++ 05_funciones.dart
@@ -2,6 +2,8 @@ void main() {
   print(greetEveryone());
 
   print('Suma: ${addTwoNumbers(10, 20)}');
+
+  print(greetPerson(name: 'Angel', message: 'Hi,'));
 }
 
 String greetEveryone() => 'Hello everyone';
@@ -9,9 +11,13 @@ String greetEveryone() => 'Hello everyone';
 int addTwoNumbers(int a, int b) => a + b;
 
 /// [tipo? opcional = valor]
-int addTwoNumbersOptional(int a, [int b = 0]) {
+int addTwoNumbersOptional(int a, [int b = 0, int? c]) {
   // b = b ?? 0;
   // b ??= 0;
 
   return a + b;
+}
+
+String greetPerson({required String name, String message = 'Hola, '}) {
+  return '$message $name';
 }

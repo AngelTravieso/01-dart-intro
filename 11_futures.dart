b8cadc9313b575b17b04fbd1ed618a317ada2785
atravieso <angeltraviesoc@gmail.com>
2023-06-20 20:19:38 -0400

    Async - Await

--- 11_futures.dart
+++ 11_futures.dart
@@ -1,18 +1,22 @@
-void main() {
+void main() async {
   print('Inicio del programa');
 
-  httpGet('https://angel-travieso.com').then((value) {
+  try {
+    final value = await httpGet('https://angel-travieso.com');
     print(value);
-  }).catchError((err) {
-    print('Error: $err');
-  });
+  } catch (err) {
+    print('Tenemos un error: $err');
+  }
 
   print('Fin del programa');
 }
 
-Future<String> httpGet(String url) {
-  return Future.delayed(const Duration(seconds: 1), () {
-    throw 'Error en la petición http';
-    // return 'Respuesta de la peticion http';
-  });
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
+
+  throw 'Error en la peticion';
+
+  // return 'Tenemos un valor de la peticion';
+  // throw 'Error en la petición http';
+  // return 'Respuesta de la peticion http';
 }

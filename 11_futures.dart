f429018a1d9809db2b652e3b0bdf9f57ecf5c8e8
atravieso <angeltraviesoc@gmail.com>
2023-06-20 20:30:17 -0400

    Try, on catch y finally

--- 11_futures.dart
+++ 11_futures.dart
@@ -3,9 +3,15 @@ void main() async {
 
   try {
     final value = await httpGet('https://angel-travieso.com');
-    print(value);
+    print('Exito: $value');
+
+    /// [err] es el String que está dentro del exception
+  } on Exception catch (err) {
+    print('Tenemos una Exception: $err');
   } catch (err) {
-    print('Tenemos un error: $err');
+    print('OOPS!! Algo terrible pasó: $err');
+  } finally {
+    print('Fin del try y catch');
   }
 
   print('Fin del programa');
@@ -14,7 +20,11 @@ void main() async {
 Future<String> httpGet(String url) async {
   await Future.delayed(const Duration(seconds: 1));
 
-  throw 'Error en la peticion';
+  // * Cae en el Exception
+  throw Exception('No hay parametros en el URL');
+
+  // * Cae en el catch
+  // throw 'Error en la peticion';
 
   // return 'Tenemos un valor de la peticion';
   // throw 'Error en la petición http';
